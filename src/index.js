ec6cca931dfe31a3090af2966f9538d05ad8a8ce
Luca <lucamug@gmail.com>
2018-01-19 23:30:47 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import './main.css';
-import { Main } from './Main.elm';
+import {
+    Main
+} from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-Main.embed(document.getElementById('root'));
-
 registerServiceWorker();
+
+const elmSpa = Main.fullscreen(localStorage.getItem("spa") || "");
+
+elmSpa.ports.urlChange.subscribe(function(title) {
+    document.body.classList.add("urlChange");
+    setTimeout(function() {
+        document.body.classList.remove("urlChange");
+    }, 100)
+    window.requestAnimationFrame(function() {
+        document.title = title;
+        document.querySelector('meta[name="description"]').setAttribute("content", title);
+    });
+});
+
+elmSpa.ports.storeLocalStorage.subscribe(function(value) {
+    localStorage.setItem("spa", value);
+});
+
+window.addEventListener("storage", function(event) {
+    if (event.storageArea === localStorage && event.key === "spa") {
+        elmSpa.ports.onLocalStorageChange.send(event.newValue);
+    }
+}, false);
